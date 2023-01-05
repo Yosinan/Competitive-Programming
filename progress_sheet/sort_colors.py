3f13f7d63ee31c36f72d0a99b6ff9061cf055a43
Yoseph <ymymym4634@gmail.com>
2023-01-06 00:02:58 +0300

    sort colors

---
+++ progress_sheet/sort_colors.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        sortedlist = []
+
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+
+                if nums[i] < nums[j]:
+
+                     temp = nums[i]
+                     nums[i] = nums[j]
+                     nums[j] = temp
+
+        return nums
