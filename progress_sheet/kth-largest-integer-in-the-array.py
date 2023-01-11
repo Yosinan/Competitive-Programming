e009023bd2a95e96935aa72a8b2a8b1a165f3472
Yoseph <ymymym4634@gmail.com>
2023-01-11 18:33:27 +0300

    kth-largest int in the array

---
+++ progress_sheet/kth-largest-integer-in-the-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def kthLargestNumber(self, nums, k):
+        """
+        :type nums: List[str]
+        :type k: int
+        :rtype: str
+        """
+
+        nums = [int(no) for no in nums]
+        nums.sort(reverse = True)
+        return str(nums[k - 1])
+            
+            
