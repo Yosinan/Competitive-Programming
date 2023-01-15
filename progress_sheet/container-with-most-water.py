9c5a2236ddf6948fbb571e0900eb81d1e06775b2
Yos <99474058+Yosinan@users.noreply.github.com>
2023-01-15 10:56:03 +0300

    Two Pointers (medium)

--- progress_sheet/container-with-most-water.py
+++ progress_sheet/container-with-most-water.py
@@ -2,7 +2,7 @@ class Solution(object):
     def maxArea(self, height):
         """
         :type height: List[int]
-        :rtype: int
+        :rtype: int 
         """
        
         left = 0
